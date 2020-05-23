5ebe6c748346bf5280beca40339a9f94b7649b19
Volovenko <vadim.volovenko@gmail.com>
2020-05-23 20:40:26 +0300

    User main page (#8)
    * add devise gem and user registration
    
    * Revert "add devise gem and user registration"
    
    This reverts commit 6b4e01efb84854081b789c4fabcc5a714b2f1220.
    
    * Revert "Revert "add devise gem and user registration""
    
    This reverts commit db930e5391308d9862dc318378ae294195d7e278.
    
    * add bootstrap, user/index template, user main page
    
    * fixed remarks, delete useless files
    
    * fix empty lines
    
    * add layouts
    
    * add custom map
    
    * add flash notice, edit custom map
    
    * add user main page, fix map bug
    
    * delete turbolinks, fix map
    
    * Delete .DS_Store
    
    Co-authored-by: TanjaZzz <x-strange@yandex.ru>

--- app/javascript/packs/custom-map.js
+++ app/javascript/packs/custom-map.js
@@ -1,8 +1,7 @@
-      var map;
-        window.initMap = function(){
-        map = new google.maps.Map(document.getElementById('map'), {
-          center: {lat: -34.397, lng: 150.644},
-          zoom: 8
-        });
-      }
-
+var map;
+  window.initMap = function(){
+      map = new google.maps.Map(document.getElementById('map'), {
+      center: {lat: 53.902237399546216, lng: 27.561849655943984},
+      zoom: 12,
+      });
+}
