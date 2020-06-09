195e6a8357aee5a56551785e896a6dbbede47c5d
TanjaZzz <51056474+TanjaZzz@users.noreply.github.com>
2020-06-09 12:37:23 +0300

    Admin (#20)
    * add devise gem and user registration
    
    * Revert "add devise gem and user registration"
    
    This reverts commit 6b4e01efb84854081b789c4fabcc5a714b2f1220.
    
    * Revert "Revert "add devise gem and user registration""
    
    This reverts commit db930e5391308d9862dc318378ae294195d7e278.
    
    * added
    
    * specs
    
    * corrected specs by commenting
    
    * add more validations, corrected all rubocop
    
    * add signin with username or email option, rubocop correction
    
    * add spec_reauests by Alex
    
    * correct after review
    
    * correct minor changes
    
    * draft rspec for signin and signout ideas
    
    * draft rspec for signin and signout ideas
    
    * RSpec for authentification and add index to username
    
    * add pry gem
    
    * Model Places with relations
    
    * Places model
    
    * add validations and factory bot
    
    * Scaffold Places model with previous settings saved
    
    * Add place_spec
    
    * add relations validations to user-places models
    
    * Update Gemfile for Heroku
    
    * Procfile
    
    * Changed Procfile
    
    * Added nested resourse to Places
    
    * delete places scss
    
    * places
    
    * create Friends model
    
    * create Friends model
    
    * add friends assosiations, seed db
    
    * add links on nav-bar
    
    * Change style and Follow for Friend words
    
    * correct rubocop partly
    
    * search by username field
    
    * search by username and email field
    
    * search by username and email field
    
    * aligned with friends branch and correct rubocop
    
    * add specs for user and friendship
    
    * correct view/show and users_spec
    
    * correct friendships controller
    
    * delete heroku
    
    * add inverse of to User model
    
    * add friendship spec for destroy action
    
    * correct after review
    
    * corrected after review with working dropdown menu
    
    * correct title
    
    * changed user factory
    
    * add Delete User button
    
    * lost file is back
    
    * adjusted with my repo
    
    * delete heroku
    
    * changes after merge
    
    * changes in model user
    
    * Delete app_env_vars.rb
    
    * rubocop correct
    
    * rubocop correct
    
    * correct spec for user
    
    * minor changes in seeds and remove redundant file
    
    * gemfile
    
    * changed show file

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -4,9 +4,10 @@ FactoryBot.define do
   factory :user do
     name { "User_#{rand(999)}" }
     username { "Username_#{rand(999)}" }
-    sequence(:email) { |n| "awesome_user_#{n}@example.com" }
+    sequence(:email) { |n| "me_user_#{n}@example.com" }
     sex { rand(1..3) }
     age { rand(1..200) }
+    is_admin { 'false' }
     confirmed_at { DateTime.now }
     confirmation_token { "dfsdfdsf#{rand(999)}" }
     after(:build) { |u| u.password_confirmation = u.password = '123456' }
