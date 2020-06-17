f8ef62d523178dc310f5e6ff070cda5768c4f448
zal-lex <zaletov.alexej@gmail.com>
2020-06-17 03:20:10 +0300

    Update mail config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -72,10 +72,10 @@ Rails.application.configure do
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default_url_options = { :host => 'my-places.site' }
-  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.delivery_method = :sendmail
   config.action_mailer.smtp_settings = {
   :address => "smtp.yandex.ru",
-  :port => 465,
+  :port => 587,
   :domain => "yandex.ru",
   :user_name => ENV['MAILER_EMAIL'],
   :password => ENV['MAILER_PASSWORD'],
